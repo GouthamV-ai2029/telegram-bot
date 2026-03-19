38f4022ee093180138752685ebdb6c668d6e8769
GouthamV-ai2029 <goutham.v.2029@gmail.com>
2026-03-19 08:13:27 +0530

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,9 +1,27 @@
 import telebot
+from flask import Flask, request
+import os
+
 TOKEN = "8748531687:AAG8cQiy95YB_lRMxp5AofzcMv6FxV1plxM"
+
 bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
+
+@app.route('/' + TOKEN, methods=['POST'])
+def webhook():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return 'OK', 200
+
+@app.route('/')
+def home():
+    return "Bot Running 🚀"
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    user_name = message.from_user
-    text = f"Welcome {user_name} to the NVault"
-    bot.send_message(message.chat.id,text)
-bot.polling()
+    bot.send_message(message.chat.id, "Hello from Render Webhook")
+
+if __name__ == "__main__":
+    PORT = int(os.environ.get('PORT', 10000))
+    app.run(host="0.0.0.0", port=PORT)
